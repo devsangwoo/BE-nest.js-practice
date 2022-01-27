c880574977d80e71335711707aa67904c3f71d31
Cho Sangwoo <dev.sangwoo@gmail.com>
2022-01-27 05:31:35 +0100

    Eliminated unused type

--- src/location/graphql/inputs/create-location.input.ts
+++ src/location/graphql/inputs/create-location.input.ts
@@ -3,21 +3,21 @@ import { InputType, Field, Float, ID } from '@nestjs/graphql';
 
 @InputType()
 export class CreateLocationInput implements ICreateLocationInput {
-  @Field((_type) => Float)
+  @Field(() => Float)
   latitude: number;
 
-  @Field((_type) => Float)
+  @Field(() => Float)
   longitude: number;
 
   @Field()
   address: string;
 
-  @Field((_type) => ID)
+  @Field(() => ID)
   country: string;
 
   @Field()
   zipCode: string;
 
-  @Field((_type) => ID)
+  @Field(() => ID)
   customer: string;
 }
